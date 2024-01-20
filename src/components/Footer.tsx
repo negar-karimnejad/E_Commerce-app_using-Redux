ed26278bbe56b725475dbaa921a30ac05496b6fc
negar <negar.karimnejad@gmail.com>
2024-01-20 21:53:24 +0330

    coding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
-import { FaArrowRotateRight } from "react-icons/fa6";
 import footer from "../assets/img/footer.png";
 
 function Footer() {
   return (
-    <FaArrowRotateRight className="container mx-auto mt-20">
+    <footer className="container mx-auto mt-20">
       <img src={footer} alt="footer image" />
-    </FaArrowRotateRight>
+    </footer>
   );
 }
 

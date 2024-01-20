cd1bc59cc8c83a82db2046fe8835f6a58e6eee81
negar <negar.karimnejad@gmail.com>
2024-01-20 23:17:10 +0330

    coding

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -2,13 +2,13 @@ import { ServiceProps } from "../types";
 
 function Service({ icon, title, subtitle }: ServiceProps) {
   return (
-    <main className="flex-1 p-5 flex justify-center md:justify-start items-center bg-gray-100 gap-2">
+    <div className="flex-1 p-5 flex items-center bg-gray-100 gap-2">
       <div className="text-4xl">{icon}</div>
-      <section>
+      <div>
         <p className="font-bold text-gray-800 text-sm">{title}</p>
         <p className="text-xs text-gray-400">{subtitle}</p>
-      </section>
-    </main>
+      </div>
+    </div>
   );
 }
 
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -5,7 +5,7 @@ function Services() {
   return (
     <div className="bg-white my-10">
       <div className="container mx-auto px-[15px]">
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-3">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
           {services.map((service) => (
             <Service key={service.id} {...service} />
           ))}
